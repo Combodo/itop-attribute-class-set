eee03bf2b564d16850a19a84b21497088c206b01
Eric Espie <eric.espie@combodo.com>
2024-11-27 15:49:34 +0100

    N°8004 - Polishing MFA Stéphane edition (denied modes rework)

--- src/Attribute/AttributeClassSet.php
+++ src/Attribute/AttributeClassSet.php
@@ -4,48 +4,79 @@
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
+use Combodo\iTop\ItopAttributeClassSet\Helper\ItopAttributeClassSetException;
+use Combodo\iTop\ItopAttributeClassSet\Helper\ItopAttributeClassSetLog;
+
+
 class AttributeClassSet extends AttributeSet
 {
 	const SEARCH_WIDGET_TYPE = self::SEARCH_WIDGET_TYPE_STRING;
 
 	public function __construct($sCode, array $aParams)
 	{
-		$this->m_sCode = $sCode;
+		ItopAttributeClassSetLog::Enable();
 		$this->aCSSClasses[] = 'attribute-class-attcode-set';
 		$aParams['allowed_values'] = new ValueSetEnumClasses($aParams['class_category'] ?? 'business', $aParams['more_values'] ?? '');
 		parent::__construct($sCode, $aParams);
 	}
 
+	/**
+	 * @return array|string[]
+	 */
 	public static function ListExpectedParams()
 	{
-		return array_merge(parent::ListExpectedParams(), ['class_category' /*, 'more_values', 'max_items'*/]);
+		return array_merge(parent::ListExpectedParams(), ['class_category', 'max_items' /*, 'more_values'*/]);
 	}
 
+	/**
+	 * @return int
+	 * @throws \Combodo\iTop\ItopAttributeClassSet\Helper\ItopAttributeClassSetException
+	 */
 	public function GetMaxSize()
 	{
-		return max(255, 15 * $this->GetMaxItems());
+		try {
+			return max(255, 15 * $this->GetMaxItems());
+		} catch (Exception $e) {
+			throw new ItopAttributeClassSetException(__FUNCTION__.': failed', 0, $e);
+		}
 	}
 
+	/**
+	 * @param \DBObject|null $oHostObject
+	 *
+	 * @return int|string|null
+	 * @throws \Combodo\iTop\ItopAttributeClassSet\Helper\ItopAttributeClassSetException
+	 */
 	public function GetDefaultValue(DBObject $oHostObject = null)
 	{
-		$sDefault = parent::GetDefaultValue($oHostObject);
-		if (!$this->IsNullAllowed() && $this->IsNull($sDefault)) {
-			// For this kind of attribute specifying null as default value
-			// is authorized even if null is not allowed
+		try {
+			$sDefault = parent::GetDefaultValue($oHostObject);
+			if (!$this->IsNullAllowed() && $this->IsNull($sDefault)) {
+				// For this kind of attribute specifying null as default value
+				// is authorized even if null is not allowed
 
-			// Pick the first one...
-			$aClasses = $this->GetAllowedValues();
-			$sDefault = key($aClasses);
+				// Pick the first one...
+				$aClasses = $this->GetAllowedValues();
+				$sDefault = key($aClasses);
+			}
+
+			return $sDefault;
+		} catch (Exception $e) {
+			throw new ItopAttributeClassSetException(__FUNCTION__.': failed', 0, $e);
 		}
-
-		return $sDefault;
 	}
 
+	/**
+	 * @return bool
+	 */
 	public function RequiresIndex()
 	{
 		return true;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function GetBasicFilterLooseOperator()
 	{
 		return '=';
@@ -62,28 +93,37 @@ class AttributeClassSet extends AttributeSet
 	 */
 	public function GetAsHTML($value, $oHostObject = null, $bLocalize = true)
 	{
-		if ($value instanceof ormSet) {
-			$value = $value->GetValues();
-		}
-		if (is_array($value)) {
-			if ($bLocalize) {
-				$aLocalizedValues = [];
-				foreach ($value as $sClassName) {
-					try {
-						$sValue = MetaModel::GetName($sClassName);
-						$aLocalizedValues[] = '<span class="attribute-set-item" data-code="'.$sClassName.'" data-label="'.$sValue.'" data-description="" data-tooltip-content="'.$sValue.'">'.$sValue.'</span>';
-					} catch (Exception $e) {
-						// Ignore bad values
-					}
-				}
-				$value = $aLocalizedValues;
+		try {
+			if ($value instanceof ormSet) {
+				$value = $value->GetValues();
 			}
-			$value = implode('', $value);
-		}
+			if (is_array($value)) {
+				if ($bLocalize) {
+					$aLocalizedValues = [];
+					foreach ($value as $sClassName) {
+						try {
+							$sValue = MetaModel::GetName($sClassName);
+							$aLocalizedValues[] = '<span class="attribute-set-item" data-code="'.$sClassName.'" data-label="'.$sValue.'" data-description="" data-tooltip-content="'.$sValue.'">'.$sValue.'</span>';
+						} catch (Exception $e) {
+							// Ignore bad values
+						}
+					}
+					$value = $aLocalizedValues;
+				}
+				$value = implode('', $value);
+			}
 
-		return '<span class="'.implode(' ', $this->aCSSClasses).'">'.$value.'</span>';
+			return '<span class="'.implode(' ', $this->aCSSClasses).'">'.$value.'</span>';
+		} catch (Exception $e) {
+			throw new ItopAttributeClassSetException(__FUNCTION__.': failed', 0, $e);
+		}
 	}
 
+	/**
+	 * @param $proposedValue
+	 *
+	 * @return bool
+	 */
 	public function IsNull($proposedValue)
 	{
 		return empty($proposedValue);
